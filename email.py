d71c6f5c7a7c7c41b6f861b1e6183c47cf48ed59
Nilesh Barla <nielspace26@gmail.com>
2019-05-20 23:35:00 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,14 +1,16 @@
 import environment #this is the module that I have created. It contains my email and password
-import smtplib 
+import smtplib #this is a library which uses gmail api to access and send emails
 
 
-userName = environment.data.user_name
+userName = environment.data.user_name 
 passWord = environment.data.password
 
-subject = 'Enquiry'
+subject = 'Enquiry' 
 content = 'Please let me know the status of the project'
 msg = f'Subject: {subject}\n\n{content}'
-with smtplib.SMTP('smtp.gmail.com', 587) as mail:
+
+
+with smtplib.SMTP('smtp.gmail.com', 587) as mail: #587 is the port number
     mail.ehlo()
     mail.starttls()
     mail.ehlo()
